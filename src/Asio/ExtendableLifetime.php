df5ab312a5fd03a81debbe0a068a0a3e11f46505
acrylic-origami <dc.lam@mail.utoronto.ca>
2017-03-29 22:02:50 -0400

    Clean some classes out for yield-callback split

--- src/Asio/ExtendableLifetime.php
+++ src/Asio/ExtendableLifetime.php
@@ -1,16 +1,14 @@
 <?hh // strict
 namespace HHReactor\Asio;
 use HHReactor\Collection\Producer;
-class ExtendableLifetime extends Recyclable<void> {
-	private Vector<Awaitable<void>> $subawaitables = Vector{};
-	public async function extend(Awaitable<void> $incoming): Awaitable<void> {
-		$this->soft_extend($incoming);
-		await \HH\Asio\later();
-	}
-	public function soft_extend(Awaitable<void> $incoming): void {
+/**
+ * Endpoint is at least as late as the last Awaitable added to it while still unresolved.
+ */
+class ExtendableLifetime extends Recyclable<mixed> {
+	private Vector<Awaitable<mixed>> $subawaitables = Vector{};
+	public function soft_extend(Awaitable<mixed> $incoming): void {
 		$this->_soft_replace(
-			Producer::from_nonblocking($this->subawaitables->add($incoming))
-				     ->get_lifetime()
+			\HH\Asio\v($this->subawaitables->add($incoming))
    	);
 	}
 }
--- src/Collection/IHaltable.php
+++
@@ -1,5 +0,0 @@
-<?hh // strict
-namespace HHReactor\Collection;
-interface IHaltable {
-	public function soft_halt(?\Exception $e = null): void;
-}
